ec98bfcb3683c5ecb8122d838877fb45d6de8e5d
Chinmayi G S <chinmayisadashiva@gmail.com>
2026-06-24 21:28:06 +0530

    Completed Builder Pattern

---
+++ BuilderPatternExample/src/Computer.java
@@ -0,0 +1,44 @@
+public class Computer {
+
+    private String CPU;
+    private int RAM;
+    private int Storage;
+
+    private Computer(Builder builder) {
+        this.CPU = builder.CPU;
+        this.RAM = builder.RAM;
+        this.Storage = builder.Storage;
+    }
+
+    public void display() {
+        System.out.println("CPU: " + CPU);
+        System.out.println("RAM: " + RAM + " GB");
+        System.out.println("Storage: " + Storage + " GB");
+    }
+
+    public static class Builder {
+
+        private String CPU;
+        private int RAM;
+        private int Storage;
+
+        public Builder setCPU(String CPU) {
+            this.CPU = CPU;
+            return this;
+        }
+
+        public Builder setRAM(int RAM) {
+            this.RAM = RAM;
+            return this;
+        }
+
+        public Builder setStorage(int Storage) {
+            this.Storage = Storage;
+            return this;
+        }
+
+        public Computer build() {
+            return new Computer(this);
+        }
+    }
+}
