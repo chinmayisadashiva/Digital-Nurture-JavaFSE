203d7bd7719f0da4fc9c89fea62639c3418ff98c
Chinmayi G S <chinmayisadashiva@gmail.com>
2026-06-25 17:44:22 +0530

    Completed JUnit Exercise 1 - Setting Up JUnit

---
+++ JUnitBasicTesting/src/test/java/com/cognizant/junit/AppTest.java
@@ -0,0 +1,12 @@
+package com.cognizant.junit;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testAddition() {
+        assertEquals(5, 2 + 3);
+    }
+}
